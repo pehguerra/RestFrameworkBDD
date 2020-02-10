80269799d8f77d280adf5ba2f02a0c7df34a4678
pedrohsguerra <pguerra@cambridgeassociates.com>
2020-02-10 13:28:33 -0300

    Added cucumber report

--- src/test/java/bdd/pguerra/cucumber/options/TestRunner.java
+++ src/test/java/bdd/pguerra/cucumber/options/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 		features= {"src/test/resources/location"}, 
 		glue={"bdd.pguerra.stepDefinitions"},
-		tags = {"@deletePlace"},
+		tags = {"@regression"},
 		plugin = {"pretty",
 //                "html:target/report/html",
 //                "junit:target/report/junit/cucumber-report.xml",
